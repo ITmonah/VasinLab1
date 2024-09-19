9d4981a6dbd872bec5cfd86f1560e7b26a1812be
ITmonah <vasin.sasha26@gmail.com>
2024-09-19 13:54:00 +0500

    fix 2

--- app/src/main/java/com/example/vasinaslab1/MainActivity.java
+++ app/src/main/java/com/example/vasinaslab1/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.vasinaslab1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
